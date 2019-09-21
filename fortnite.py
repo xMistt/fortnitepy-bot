ddba71cad0399ca9e9172b9e02c94c4715426b51
xMistt <nitroplug21@gmail.com>
2019-09-21 10:14:37 +0100

    Added console support!
    The days of having to whisper from your PC are no more, if you now type in party chat on console, the bot will understand your messages and put them into action.

--- fortnite.py
+++ fortnite.py
@@ -89,4 +89,72 @@ async def event_friend_message(message):
 
         await message.reply('Pickaxe set to' + message.content + '!')
 
+@client.event
+async def event_party_message(message):
+    # only type these if you're alone in your lobby + you're on console.
+    print('Received message from {0.author.display_name} | Content: "{0.content}"'.format(message))
+
+    if message.content == "!purpleskull":
+        variants = client.user.party.me.create_variants(
+           clothing_color=1
+        )
+
+        await client.user.party.me.set_outfit(
+            asset='CID_030_Athena_Commando_M_Halloween',
+            variants=variants
+        )
+
+        await message.reply('Skin set to Purple Skull Trooper!')
+
+    if "CID_" in message.content:
+        await client.user.party.me.set_outfit(
+            asset=message.content
+        )
+
+    if message.content == "!checkeredrenegade":
+
+        variants = client.user.party.me.create_variants(
+           material=2
+        )
+
+        await client.user.party.me.set_outfit(
+            asset='CID_028_Athena_Commando_F',
+            variants=variants
+        )
+
+        await message.reply('Skin set to' + message.content + '!')
+
+    if "EID_" in message.content:
+        await client.user.party.me.set_emote(
+            asset=message.content
+        )
+        await message.reply('Emote set to' + message.content + '!')
+        
+    if "!stop" in message.content:
+        await client.user.party.me.set_emote(
+            asset="StopEmote"
+        )
+        await message.reply('Stopped emoting.')
+
+    if "BID_" in message.content:
+        await client.user.party.me.set_backpack(
+            asset=message.content
+        )
+
+        await message.reply('Backbling set to' + message.content + '!')
+
+    if "!help" in message.content:
+        await client.user.party.me.set_backpack(
+            asset=message.content
+        )
+
+        await message.reply('My commands are; !purpleskull, CID_, EID_, BID_ !stop & !help')
+
+    if "PICKAXE_ID_" in message.content:
+        await client.user.party.me.set_pickaxe(
+            asset=message.content
+        )
+
+        await message.reply('Pickaxe set to' + message.content + '!')
+
 client.run()
