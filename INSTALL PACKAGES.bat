3ae4171bf598d9c6e28161f914605948ceb36185
xMistt <nitroplug21@gmail.com>
2019-10-06 12:09:52 +0100

    Made install packages more informational.

--- INSTALL PACKAGES.bat
+++ INSTALL PACKAGES.bat
@@ -1,3 +1,10 @@
+@echo off
+ECHO Installing the required packages for the bot!
+TIMEOUT 3
+
 py -3 -m pip install -U fortnitepy
 py -3 -m pip install -U aiohttp
-py -3 -m pip install -U colorama
+
+ECHO Done! Now run START BOT.bat
+PAUSE
+
