31a64e1c8cce89ff2a8b1712b96fd74e55334b57
Classic36 <classic36mc@gmail.com>
2019-10-27 00:24:44 +0100

    Added correct name for AsyncBenBot
    Was previously "BenBot-async" as per README

--- INSTALL PACKAGES.bat
+++ INSTALL PACKAGES.bat
@@ -5,7 +5,7 @@ TIMEOUT 3
 py -3 -m pip install -U fortnitepy
 py -3 -m pip install -U aiohttp
 py -3 -m pip install -U colorama
-py -3 -m pip install -U BenBot-async
+py -3 -m pip install -U AsyncBenBot
 
 
 ECHO Done! Now run START BOT.bat
