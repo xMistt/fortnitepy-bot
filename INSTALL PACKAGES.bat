fecb6774d18b622294d6e0931ba48f7aed64eea9
Classic36 <classic36mc@gmail.com>
2019-10-27 00:15:07 +0100

    Fix/update install packages

--- INSTALL PACKAGES.bat
+++ INSTALL PACKAGES.bat
@@ -2,10 +2,10 @@
 ECHO Installing the required packages for the bot!
 TIMEOUT 3
 
-python3 -m pip install -U fortnitepy
-python3 -m pip install -U aiohttp
-python3 -m pip install -U colorama
-python3 -m pip install -U BenBot-async
+py -3 -m pip install -U fortnitepy
+py -3 -m pip install -U aiohttp
+py -3 -m pip install -U colorama
+py -3 -m pip install -U BenBot-async
 
 
 ECHO Done! Now run START BOT.bat
