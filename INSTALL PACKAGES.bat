d2ecaa091349938ddffb23301a6fbd43bd8ab619
Oli <55298769+xMistt@users.noreply.github.com>
2019-11-08 22:39:50 +0000

    Fixed all errors.

--- INSTALL PACKAGES.bat
+++ INSTALL PACKAGES.bat
@@ -6,6 +6,7 @@ py -3 -m pip install -U fortnitepy
 py -3 -m pip install -U aiohttp
 py -3 -m pip install -U colorama
 py -3 -m pip install -U BenBotAsync
+py -3 -m pip install -U livejson
 
 
 ECHO Done! Now run START BOT.bat
