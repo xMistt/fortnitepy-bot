a8c6458a2e5abb2c9f86fb09394b445913830bf7
Oli <55298769+xMistt@users.noreply.github.com>
2019-12-03 22:21:58 +0000

    Added hotfix for BenBotAsync.

--- INSTALL PACKAGES.bat
+++ INSTALL PACKAGES.bat
@@ -5,7 +5,7 @@ TIMEOUT 3
 py -3 -m pip install -U fortnitepy
 py -3 -m pip install -U aiohttp
 py -3 -m pip install -U colorama
-py -3 -m pip install -U BenBotAsync
+py -3 -m pip install -U BenBotAsync==1.0.3
 
 
 ECHO Done! Now run START BOT.bat
