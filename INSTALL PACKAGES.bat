578f80caaf981da0da579e4d1846c013d1e719f1
KaosDrip <55036780+KaosDrip@users.noreply.github.com>
2019-12-09 19:10:58 -0600

    Update INSTALL PACKAGES.bat

--- INSTALL PACKAGES.bat
+++ INSTALL PACKAGES.bat
@@ -5,7 +5,7 @@ TIMEOUT 3
 py -3 -m pip install -U fortnitepy
 py -3 -m pip install -U aiohttp
 py -3 -m pip install -U colorama
-py -3 -m pip install -U BenBotAsync==1.0.2
+py -3 -m pip install -U BenBotAsync==1.0.1
 
 
 ECHO Done! Now run START BOT.bat
