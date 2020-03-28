1bed0a22723d98d53471fb181fe756efc76c1a45
jande460 <50678826+jande460@users.noreply.github.com>
2020-03-28 14:40:31 -0700

    Update fortnite.py

--- fortnite.py
+++ fortnite.py
@@ -2,7 +2,7 @@
 
 """
 “Commons Clause” License Condition v1.0
-Copyright Oli 2019
+Copyright Oli 2019-2020
 
 The Software is provided to you by the Licensor under the
 License, as defined below, subject to the following condition.
