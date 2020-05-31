6abfda6449a989aff35041c0cac5d571ba37d7a4
Oli <55298769+xMistt@users.noreply.github.com>
2020-05-31 13:07:14 +0100

    Exchange code possible to get again.

--- fortnite.py
+++ fortnite.py
@@ -219,7 +219,7 @@ client = commands.Bot(
     auth=fortnitepy.AdvancedAuth(
         email=data['email'],
         password=data['password'],
-        prompt_exchange_code=False,
+        prompt_exchange_code=True,
         delete_existing_device_auths=True,
         **device_auth_details
     ),
