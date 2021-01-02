c3b45ad46d0cba5f8f3e452b626b9f6869c1db79
Oli <55298769+xMistt@users.noreply.github.com>
2021-01-02 23:40:38 +0000

    Python 3.6.0 -> 3.7.0.

--- START BOT.bat
+++ START BOT.bat
@@ -18,15 +18,13 @@ goto:eof
 
 :errorNoPython
 echo Error^: Python not installed or not added to PATH.
-:: set mypath=%cd%
-:: bitsadmin.exe /transfer "InstallPython" https://www.python.org/ftp/python/3.6.0/python-3.6.0-amd64.exe %mypath%\PythonInstaller.exe
 
-IF EXIST "python-3.6.0-amd64.exe" (
+IF EXIST "python-3.7.0-amd64.exe" (
     echo Python Installer is already installed, install and/or add Python to PATH
 ) ELSE (
     echo Installing Python Installer now, this will take a minute or 2.
-    powershell -Command "(New-Object Net.WebClient).DownloadFile('https://www.python.org/ftp/python/3.6.0/python-3.6.0-amd64.exe', 'python-3.6.0-amd64.exe')"
-    powershell -Command "Invoke-WebRequest https://www.python.org/ftp/python/3.6.0/python-3.6.0-amd64.exe -OutFile python-3.6.0-amd64.exe"   
+    powershell -Command "(New-Object Net.WebClient).DownloadFile('https://www.python.org/ftp/python/3.7.0/python-3.7.0-amd64.exe', 'python-3.7.0-amd64.exe')"
+    powershell -Command "Invoke-WebRequest https://www.python.org/ftp/python/3.7.0/python-3.7.0-amd64.exe -OutFile python-3.7.0-amd64.exe"   
     echo Python Installer is now installed, install and/or add Python to PATH.
 )
 
